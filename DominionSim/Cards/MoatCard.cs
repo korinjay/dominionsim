ca2e0574d54a6219def58f7254f108d071341a45
Colin C. <magroader@gmail.com>
2010-03-17 01:36:35 -0400

    Adding in Moats, along with some basic strategies for trying out just 1 card

--- DominionSim/Cards/MoatCard.cs
+++ DominionSim/Cards/MoatCard.cs
@@ -10,5 +10,17 @@ namespace DominionSim.Cards
         public MoatCard() : base(CardList.Moat, ReactionAction, 2, 2, 0, 0, 0, 0)
         {
         }
+
+        /// <summary>
+        /// Perform a reaction
+        /// </summary>
+        /// <param name="attacker">One performing the attack</param>
+        /// <param name="victim">One getting hit by the attack</param>
+        /// <param name="supply">The supply</param>
+        /// <returns>True if the attack was successfully prevented from affecting the victim</returns>
+        public override bool ExecuteReaction(Player attacker, Player victim, Supply supply)
+        {
+            return true;
+        }
     }
 }
