8dc3673d63e42cc1e488f141059c4a727403a13e
Mark Haines <lilserf@gmail.com>
2010-03-17 00:37:57 -0400

    Fixed bug with Mine if you HAD no treasure.

--- DominionSim/Cards/MineCard.cs
+++ DominionSim/Cards/MineCard.cs
@@ -16,14 +16,23 @@ namespace DominionSim.Cards
         {
             base.ExecuteCard(p, supply);
 
-            string trashing = p.Strategy.ChooseCardsToTrash(p.GetFacade(), 1, 1, CardType.Treasure, supply).ElementAt(0);
-            Card trashCard = CardList.Cards[trashing];
+            if (p.Hand.Select((c) => CardList.Cards[c].Type)       // Get our hand as Types
+                     .Where((t) => (t & CardType.Treasure) != 0)   // Filter only Treasure
+                     .Count() > 0)                                  // And see if we ended up with any
+            {
+                string trashing = p.Strategy.ChooseCardsToTrash(p.GetFacade(), 1, 1, CardType.Treasure, supply).ElementAt(0);
+                Card trashCard = CardList.Cards[trashing];
 
-            p.TrashCard(trashing);
+                p.TrashCard(trashing);
 
-            string gaining = p.Strategy.ChooseCardToGain(p.GetFacade(), 0, trashCard.Cost + 3, CardType.Treasure, supply);
+                string gaining = p.Strategy.ChooseCardToGain(p.GetFacade(), 0, trashCard.Cost + 3, CardType.Treasure, supply);
 
-            p.GainCard(gaining);
+                p.GainCard(gaining);
+            }
+            else
+            {
+                // Dummy playing this card had no treasure!
+            }
         }
     }
 }
