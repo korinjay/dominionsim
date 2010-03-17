93df7cae536582d7d6ab4fb175f625b742e756cc
Mark Haines <lilserf@gmail.com>
2010-03-17 02:42:36 -0400

    Clean up merge error in MilitiaCard.

--- DominionSim/Cards/MilitiaCard.cs
+++ DominionSim/Cards/MilitiaCard.cs
@@ -26,7 +26,7 @@ namespace DominionSim.Cards
 
                 if (!HandleAttackReactions(p, opponent, supply))
                 {
-                    var discards = opponent.Strategy.ChooseCardsToDiscard(opponent.GetFacade(), numToDiscard, numToDiscard, supply);
+                    var discards = opponent.Strategy.ChooseCardsToDiscard(opponent.GetFacade(), numToDiscard, numToDiscard, CardType.Any, supply);
 
                     if (discards.Count() < numToDiscard)
                     {
