75a4f4c46fe45eb54d6876d2703fa81ed02bd1cd
Colin C. <Colin C.@localhost>
2010-03-21 13:30:57 -0400

    Adding in a couple simpler Caravan strats.

--- DominionSim/Strategy/Caravan.cs
+++ DominionSim/Strategy/Caravan.cs
@@ -5,10 +5,15 @@ using System.Text;
 
 namespace DominionSim.Strategy
 {
-    class Caravan : BuyOneCard
+    class Caravan3 : BuyOneCard
     {
-        public Caravan() : base(CardList.Caravan, 10)
-        {
-        }
+        public Caravan3() : base(CardList.Caravan, 3) { }
+        public static string GetDisplayName() { return "Caravan - Buy 3"; }
+    }
+
+    class Caravan4 : BuyOneCard
+    {
+        public Caravan4() : base(CardList.Caravan, 4) { }
+        public static string GetDisplayName() { return "Caravan - Buy 4"; }
     }
 }
