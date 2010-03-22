9a735bf033ef90835f1c5dfd4da369c0133ee1d3
Colin C. <Colin C.@localhost>
2010-03-21 21:22:23 -0400

    Backed out changeset: 1c38333501db

--- DominionSim/Strategy/Smithy.cs
+++ DominionSim/Strategy/Smithy.cs
@@ -25,11 +25,10 @@ namespace DominionSim.Strategy
         /// Override Smithy purchasing logic.  Attempt to keep the number of Smithies in the deck intelligent
         /// </summary>
         /// <param name="p">Player</param>
-        /// <param name="s">Supply</param>
         /// <returns>Whether we bought one</returns>
-        protected override bool AttemptBuyCard(PlayerFacade p, Supply s)
+        protected override bool AttemptBuyCard(PlayerFacade p)
         {
-            if (AbleToBuy(p, s, mCardToBuy))
+            if (CanAfford(p, mCardToBuy))
             {
                 int numSmithies = p.GetDeck().Where(vi => vi.CardId == mCardToBuy).Count();
                 int numCards = p.GetDeck().Count();
